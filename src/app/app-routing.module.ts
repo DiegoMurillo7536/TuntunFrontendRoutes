69f6cfefc03277728758634fd29dc388003e4bec
Diego Murilo <diegoa31052004@gmail.com>
2022-11-20 14:37:22 -0500

    Rutas en la parte del cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddressComponent } from './address/address.component';
-import { CartComponent } from './cart/cart.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditAddressComponent } from './edit-address/edit-address.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { OrdersComponent } from './orders/orders.component';
-import { ProductsingleComponent } from './productsingle/productsingle.component';
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-import { ShopComponent } from './shop/shop.component';
-import { SignupComponent } from './signup/signup.component';
+import { AddressComponent } from './client/address/address.component';
+import { CartComponent } from './client/cart/cart.component'; 
+import { CheckoutComponent } from './client/checkout/checkout.component'; 
+import { DashboardComponent } from './client/dashboard/dashboard.component'; 
+import { EditAddressComponent } from './client/edit-address/edit-address.component';
+import { ForgotPasswordComponent } from './client/forgot-password/forgot-password.component'; 
+import { HomeComponent } from './client/home/home.component'; 
+import { LoginComponent } from './client/login/login.component'; 
+import { OrdersComponent } from './client/orders/orders.component'; 
+import { ProductsingleComponent } from './client/productsingle/productsingle.component'; 
+import { ProfileDetailsComponent } from './client/profile-details/profile-details.component'; 
+import { ShopComponent } from './client/shop/shop.component'; 
+import { SignupComponent } from './client/signup/signup.component'; 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/Cliente/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsingleComponent } from './productsingle/productsingle.component';
-import { CartComponent } from './cart/cart.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { ShopComponent } from './shop/shop.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { OrdersComponent } from './orders/orders.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-import { AddressComponent } from './address/address.component';
-import { EditAddressComponent } from './edit-address/edit-address.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { AppRoutingModule } from './app-routing.module';
-
+import { HeaderComponent } from './client/header/Cliente/header.component'; 
+import { FooterComponent } from './client/footer/footer.component'; 
+import { HomeComponent } from './client/home/home.component'; 
+import { ProductsingleComponent } from './client/productsingle/productsingle.component'; 
+import { CartComponent } from './client/cart/cart.component'; 
+import { CheckoutComponent } from './client/checkout/checkout.component'; 
+import { ShopComponent } from './client/shop/shop.component'; 
+import { DashboardComponent } from './client/dashboard/dashboard.component'; 
+import { OrdersComponent } from './client/orders/orders.component'; 
+import { LoginComponent } from './client/login/login.component'; 
+import { SignupComponent } from './client/signup/signup.component'; 
+import { ForgotPasswordComponent } from './client/forgot-password/forgot-password.component'; 
+import { ProfileDetailsComponent } from './client/profile-details/profile-details.component';
+import { AddressComponent } from './client/address/address.component'; 
+import { EditAddressComponent } from './client/edit-address/edit-address.component'; 
+import { SlickCarouselModule } from 'ngx-slick-carousel'; 
+import { AppRoutingModule } from './app-routing.module'; 
 @NgModule({
   declarations: [
     AppComponent,
